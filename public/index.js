c57130ddd42fe731402b7d39f73f34afb5b70767
nerdfleeks <mawayalebo@users.noreply.github.com>
2021-01-17 13:48:39 -0800

    adding calculations for both views and adding unique features

--- public/index.js
+++ public/index.js
@@ -12,15 +12,24 @@ var noPassengerOut = document.querySelector("#noPassengerOut");
 
 var changeButton = document.querySelector('#changeButton');
 
-var changeData = [];
-var dataId = 0;
+
 
 var recentOut = document.querySelector('#recentOut');
 var recentContainer = document.querySelector('#recentContainer');
+var recentContainerForSlider = document.querySelector('.recentContainerForSlider');
+var recentOutForSlider = document.querySelector("#recentOutForSlider");
+
+var line = document;
 
 var lock = document.querySelector("#lock");
+var lockSlider = document.querySelector("#lockSlider");
 
-var who = "";
+var navWrapper = document.querySelector("#navWrapper");
+
+
+taxiFareOut.innerHTML = taxiFareSlider.value;
+amountHandedOut.innerHTML = amountHandedSlider.value;
+noPassengerOut.innerHTML = noPassengerSlider.value;
 
 taxiFareSlider.oninput = ()=>{
     taxiFareOut.innerHTML = taxiFareSlider.value;
@@ -40,74 +49,34 @@ var change = ( taxiFare, amountHanded, noPassenger)=>{
 
 };
 
-
-
-
-
 changeButton.onclick =(e)=>{
     e.preventDefault();
     var checkChange = change(taxiFare.value, amountHanded.value, noPassenger.value);
     if(checkChange > 0){
-        recentContainer.style.display = "block";
-        dataId  += 1;
-        if(noPassenger.value == 1){
-            who="passenger";
-        }else{
-            who="passengers";
-        }
-        changeData.push({noPassenger:noPassenger.value, amountHanded:amountHanded.value, change:checkChange, id:dataId, color:"green-text"});
-        console.log(changeData);
-        const mappedData = changeData.map(data=>{
-            return(`
-                <div class="center hoverable ${data.color}" key=${data.id}>
-                   <p >${data.id} - the change for ${data.noPassenger} ${who} who handed R${data.amountHanded} is R${data.change}</p>
-                </div>
-            `);
-        });
-        recentOut.innerHTML = mappedData;
-    };
-    if(checkChange < 0){
-        recentContainer.style.display = "block";
-        dataId +=1;
-        if(noPassenger.value == 1){
-            who="passenger";
-        }else{
-            who="passengers";
-        }
-        changeData.push({noPassenger:noPassenger.value, amountHanded:amountHanded.value, change:checkChange, id:dataId, color:"red-text"});
-        console.log(changeData);
-        const mappedData = changeData.map(data=>{
-            return(`
-                <div class="center hoverable ${data.color}" key=${data.id}>
-                   <p >${data.id} - the ${data.noPassenger} ${who} who handed you R${data.amountHanded} must give you R${ Math.abs(data.change) } extra</p>
-                </div>
-            `);
-        });
-        recentOut.innerHTML = mappedData;
-
-    };
-    if( checkChange == 0){
-        recentContainer.style.display = "block";
-        dataId +=1;
-        if(noPassenger.value == 1){
-            who="passenger";
-        }else{
-            who="passengers";
-        }
-        changeData.push({noPassenger:noPassenger.value, amountHanded:amountHanded.value, change:checkChange, id:dataId, color:"grey-text"});
-        console.log(changeData);
-        const mappedData = changeData.map(data=>{
-            return(`
-                <div class="center hoverable ${data.color}" key=${data.id}>
-                   <p >${data.id} - the ${data.noPassenger} ${who} who handed you R${data.amountHanded} has paid the full amount.</p>
-                </div>
-            `);
-        });
-        recentOut.innerHTML = mappedData;
+        recentContainer.style.display = "block"
+        recentOut.innerHTML +=`<li class="collection-item green-text" id="line">the Change for ${noPassenger.value} ${checkWho(noPassenger.value)} who handed you R${amountHanded.value} is R${checkChange}</li>`;
     }
-
+    if(checkChange == 0){
+        recentContainer.style.display = "block"
+        recentOut.innerHTML +=`<li class="collection-item grey-text" id="line">${noPassenger.value} ${checkWho(noPassenger.value)} who handed you R${amountHanded.value} paid full amount.</li>`;
+    }
+    if(checkChange < 0){
+        recentContainer.style.display = "block"
+        recentOut.innerHTML +=`<li class="collection-item red-text " id="line">${noPassenger.value} ${checkWho(noPassenger.value)} who handed you R${amountHanded.value} must hand you R${Math.abs(checkChange)} extra.</li>`;
+    }
+};
+function checkWho(passengers){
+    if(passengers == 1){
+        return "passenger";
+    }
+    return "passengers";
 };
 
+line.addEventListener('click',(e)=>{
+    if(e.target.id == "line"){
+        e.target.style.textDecoration = "line-through";
+    };
+});
 lock.addEventListener('click',(e)=>{
 
     if(e.target.id == 'lock'){
@@ -119,7 +88,50 @@ lock.addEventListener('click',(e)=>{
         taxiFare.setAttribute("disabled","");
     };
 });
+lockSlider.addEventListener('click',(e)=>{
+    if(e.target.textContent == "lock_open"){
+        lockSlider.innerHTML = `<i id="LockSlider" class="small material-icons col s2 m2">lock</i>`;
+        taxiFareSlider.setAttribute("disabled","");
+        
+    }else{
+        lockSlider.innerHTML = `<i id="LockSlider" class="small material-icons col s2 m2">lock_open</i>`;
+        taxiFareSlider.removeAttribute("disabled");
+    }
+     
 
+});
+
+var body = document.querySelector("body");
+var forDriver = document.querySelector("#forDriver");
+var changeOut = document.querySelector("#changeOut");
+
+changeSliderButton.addEventListener('click',(e)=>{
+    e.preventDefault();
+    var checkChange = change(taxiFareSlider.value, amountHandedSlider.value, noPassengerSlider.value);
+    if(checkChange > 0){
+        recentContainerForSlider.style.display = "block"
+        recentOutForSlider.innerHTML +=`<li class="collection-item teal white-text" id="line">the Change for ${noPassengerSlider.value} ${checkWho(noPassengerSlider.value)} who handed you R${amountHandedSlider.value} is R${checkChange}.<i class="material-icons right tiny green-text lighten-3">lens</i></li>`;
+        body.setAttribute("class","green");
+        forDriver.textContent = `Amount for driver: R${amountHandedSlider.value - checkChange }`;
+        changeOut.textContent =`Change for ${checkWho(noPassengerSlider.value)}: R${checkChange}`;
+    }
+    if(checkChange == 0){
+        recentContainerForSlider.style.display = "block"
+        recentOutForSlider.innerHTML +=`<li class="collection-item teal white-text" id="line">${noPassengerSlider.value} ${checkWho(noPassengerSlider.value)} who handed you R${amountHandedSlider.value} paid the full amount.<i class="material-icons right tiny grey-text">lens</i></li>`;
+        body.setAttribute("class","grey");
+        forDriver.textContent = `Amount for driver: R${amountHandedSlider.value }`;
+        changeOut.textContent =`No Change`;
+    }
+    if(checkChange < 0){
+        recentContainerForSlider.style.display = "block"
+        recentOutForSlider.innerHTML +=`<li class="collection-item teal white-text" id="line">${noPassengerSlider.value} ${checkWho(noPassengerSlider.value)} who handed you R${amountHandedSlider.value} must hand you R${Math.abs(checkChange)} extra.<i class="material-icons right tiny red-text">lens</i></li>`;
+        body.setAttribute("class","red");
+        forDriver.textContent = `Amount for driver: R${amountHandedSlider.value }`;
+        changeOut.textContent =`on top of the amount for driver the driver would need R${Math.abs(checkChange)} more.`;
+    }
+
+
+});
 
 
 
